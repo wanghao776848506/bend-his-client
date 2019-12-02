98da48cb1be3b7e6b3305e054f29e6b4e9ed3d29
liu_debin <liu_debin@finebei.com>
2019-12-02 16:53:21 +0800

    update

--- bend-his-client-web/src/main/java/com/bend/his/web/config/SwaggerConfig.java
+++ bend-his-client-web/src/main/java/com/bend/his/web/config/SwaggerConfig.java
@@ -4,8 +4,6 @@ import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrap
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -16,17 +14,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+public class SwaggerConfig {
 
     @Bean
     public Docket swaggerSpringMvcPlugin() {
-        return new Docket(DocumentationType.SWAGGER_2).
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).
                 select().
                 apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).
                 build();
@@ -37,7 +29,6 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .title("")
                 .description("")
                 .termsOfServiceUrl("")
-                .contact("")
                 .version("1.0")
                 .build();
     }
