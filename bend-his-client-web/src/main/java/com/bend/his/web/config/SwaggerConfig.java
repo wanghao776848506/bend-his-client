2a4aca83880b085ab8778b5120cecd22e54bb1fb
liu_debin <liu_debin@finebei.com>
2019-12-02 15:05:54 +0800

    update

--- bend-his-client-web/src/main/java/com/bend/his/web/config/SwaggerConfig.java
+++ bend-his-client-web/src/main/java/com/bend/his/web/config/SwaggerConfig.java
@@ -1,25 +1,28 @@
 package com.bend.his.web.config;
 
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
-import com.google.common.collect.Ordering;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Operation;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.OperationsSorter;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
     @Bean
     public Docket swaggerSpringMvcPlugin() {
@@ -28,6 +31,7 @@ public class SwaggerConfig {
                 apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).
                 build();
     }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("")
